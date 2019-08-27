98cd66b00be5fc7043e807d336a452fe13e89f3a
hanyu <you@example.com>
2019-08-27 11:37:34 +0800

    增加中间件

--- api/routers.go
+++ api/routers.go
@@ -8,7 +8,12 @@ import (
 func setupRouter() *gin.Engine {
 
 	//创建初始路由
-	router := gin.New()
+	router := gin.New()//创建一个无中间件的路由
+	// 如果要创建一个带日志和恢复的中间件路由  router := gin.Default()
+
+	router.Use(gin.Logger())// 使用日志中间件
+
+	router.Use(gin.Recovery())// 使用恢复中间件
 
 	// 注册api路由
 	RegisterRoutes.RegisterApiRoutes(router)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rean23/golangLearnNotes
+
+go 1.12
